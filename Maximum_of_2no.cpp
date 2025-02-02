063f1b3072dedcd8d0f1c183388ac20208c8dc55
Samiksha-mane <samikshamane2005@gmail.com>
2025-02-02 21:19:26 +0530

    Add files via upload

---
+++ Maximum_of_2no.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num1,num2;
+	cout<<"\n Enter the 1st  number :";
+	cin>>num1;
+	cout<<"\n Enter the 2nd  number :";
+	cin>>num2;
+	if(num1>num2)
+		cout<<num1<<"\n  is maximum number :";
+	else
+		cout<<num2<<"  is maximum number.";
+	return 0;
+}
