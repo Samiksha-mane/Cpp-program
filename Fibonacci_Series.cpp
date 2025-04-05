46e0e3944d2f3bd6f4ffbaf0963d4c16490893db
Samiksha-mane <samikshamane2005@gmail.com>
2025-04-05 20:33:01 +0530

    Add files via upload

---
+++ Fibonacci_Series.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+    int first = 0, second = 1, next;
+
+    cout << "Enter the number of terms: ";
+    cin >> n;
+
+    cout << "Fibonacci Series: ";
+
+    for (int i = 1; i <= n; ++i) 
+	{
+        cout << first << " ";
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    cout << endl;
+    return 0;
+}
+
